706b02f7ed5239d15367133c171748e0a46a8f40
gentawibowo <tetrahedral@gmail.com>
2021-07-18 09:30:47 +0700

    initial commit

---
+++ setup.sh
@@ -0,0 +1,15 @@
+yes n | paru
+paru timeshif
+sudo timeshift --create
+paru
+git clone https://gitlab.com/dwt1/dotfiles
+paru alacritty
+paru fish
+paru starship
+paru shell-color-scripts
+paru exa
+paru neovim
+paru lolcat
+paru brave
+paru docker
+paru docker-compose
